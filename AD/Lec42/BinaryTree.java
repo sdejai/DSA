02ed6fbb834bbd842bd24fd48892cdc849dbe258
sdejai <sde.jai.java@gmail.com>
2025-04-14 11:43:13 +0530

    Binary Tree Creating

---
+++ AD/Lec42/BinaryTree.java
@@ -0,0 +1,57 @@
+package Tree;
+
+import java.util.Scanner;
+
+public class BinaryTree {
+	class Node {
+		int data;
+		Node left;
+		Node right;
+	}
+
+	private Node root;
+	Scanner sc = new Scanner(System.in);
+
+	public BinaryTree() {
+		root = createTree();
+	}
+
+	public Node createTree() {
+		int element = sc.nextInt();
+		Node newNode = new Node();
+		newNode.data = element;
+		boolean leftNodeExist = sc.nextBoolean();
+		if (leftNodeExist) {
+			newNode.left = createTree();
+		}
+		boolean rightNodeExist = sc.nextBoolean();
+		if (rightNodeExist) {
+			newNode.right = createTree();
+		}
+		return newNode;
+	}
+
+	public void display() {
+		display(root);
+	}
+
+	private void display(Node newNode) {
+		if (newNode == null) {
+			return;
+		}
+		String s = "<----" + newNode.data + "---->";
+		if (newNode.left != null) {
+			s = newNode.left.data + s;
+		} else {
+			s = "." + s;
+		}
+		if (newNode.right != null) {
+			s = s + newNode.right.data;
+		} else {
+			s = s + ".";
+		}
+		System.out.println(s);
+		display(newNode.left);
+		display(newNode.right);
+	}
+}
---
+++ AD/Lec42/BinaryTreeMain.java
@@ -0,0 +1,8 @@
+package Tree;
+
+public class BinaryTreeMain {
+	public static void main(String[] args) {
+		BinaryTree bt = new BinaryTree();
+		bt.display();
+	}
+}
